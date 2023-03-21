34316b262ceaa050bf58d618499e704281c4024e
fluffyshark <robin.yjord1@gmail.com>
2023-03-21 03:48:20 +0100

    Special Effects - Implemented scroll animation on WomanPopular.

--- components/layouts/womenPopular/WomenPopular.tsx
+++ components/layouts/womenPopular/WomenPopular.tsx
@@ -17,15 +17,34 @@ import pinkBtn from "../../../app/assets/images/pinkBtn.png"
 type Props = {}
 
 const WomenPopular = (props: Props) => {
+
+  window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset;
+   
+    console.log(scroll)
+
+    if (scroll > 599) {(document.getElementById("womanPopularTitle") as HTMLFormElement).classList.add("fadeInTop");}
+
+    if (scroll > 699) {(document.getElementById("womanPromotion") as HTMLFormElement).classList.add("fadeInTop");}
+
+    if (scroll > 900) {const elements = document.getElementsByClassName("womenItem1"); for (let i = 0; i < elements.length; i++) {elements[i].classList.add("fadeInTop");}}
+    
+    if (scroll > 1200) {const elements = document.getElementsByClassName("womenItem2");for (let i = 0; i < elements.length; i++) {elements[i].classList.add("fadeInTop");}}
+
+    if (scroll > 1500) {(document.getElementById("womanAuction") as HTMLFormElement).classList.add("fadeInTop");}
+  })
+
+
+
   return (
     <div className='womenPopular'>
         <div className="container">
             
-            <div className="title">
+            <div id="womanPopularTitle" className="title animate">
               <p>WOMEN</p>
               <p>POPULAR SHOES</p>
             </div>
-            <div className="womenPromotion">
+            <div id="womanPromotion" className="womenPromotion animate">
               <div className="container">
                 <Image src={special_shoe01} alt="" />
                 <div className="text">
@@ -42,41 +61,41 @@ const WomenPopular = (props: Props) => {
             </div>
             
             <div className="shoesContainer">
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem1">
                 <Image src={shoe4} alt="" />
                 <div className="itemTexts"><p>ForeverRUN Nitro</p><p>$ 180.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem1">
                 <Image src={shoe5} alt="" />
                 <div className="itemTexts"><p>Run XX WNS Nitro</p><p>$ 100.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem1">
                 <Image src={shoe6} alt="" />
                 <div className="itemTexts"><p>Deviate 2 Nitro</p><p>$ 139.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem1">
                 <Image src={shoe7} alt="" />
                 <div className="itemTexts"><p>Electrify 2 Nitro</p><p>$ 120.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem2">
                 <Image src={shoe8} alt="" />
                 <div className="itemTexts"><p>Ghost 15</p><p>$ 140.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem2">
                 <Image src={shoe9} alt="" />
                 <div className="itemTexts"><p>Grand 360</p><p>$ 290.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem2">
                 <Image src={shoe10} alt="" />
                 <div className="itemTexts"><p>Glycerin 20</p><p>$ 150.00</p></div>
               </div>
-              <div className="itemContainer">
+              <div className="itemContainer animate womenItem2">
                 <Image src={shoe11} alt="" />
                 <div className="itemTexts"><p>Adredaline GTS 22</p><p>$ 140.00</p></div>
               </div>
             </div>
 
-          <div className="womanAuction">
+          <div id="womanAuction" className="womanAuction animate">
             <Image src={womenAuction} alt="" />
             <div className="textContainer">
               <p>SHRINKING PRICE</p>
